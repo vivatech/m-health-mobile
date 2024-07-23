ef5792dbc63363b9d3a67fefdc745e271528013b
msham <m.shamshad@vivatelecoms.com>
2024-07-23 12:30:57 +0530

    fix storage issue

--- src/main/java/com/service/mobile/MobileServiceApplication.java
+++ src/main/java/com/service/mobile/MobileServiceApplication.java
@@ -1,12 +1,15 @@
 package com.service.mobile;
 
+import com.service.mobile.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class MobileServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/service/mobile/storage/StorageProperties.java
+++ src/main/java/com/service/mobile/storage/StorageProperties.java
@@ -3,8 +3,7 @@ package com.service.mobile.storage;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Service;
-
+@Data
 @ConfigurationProperties(value = "storage")
 public class StorageProperties {
 
@@ -12,12 +11,6 @@ public class StorageProperties {
      * Folder location for storing files
      */
     @Value("${storage.location}")
-    public String location;
-    public String getLocation() {
-        return location;
-    }
+    private String location = "upload-dir";
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
