f812414ada275792c2cca0d5135104b20fccb45d
bhupendra1404office <129068431+bhupendra1404office@users.noreply.github.com>
2024-07-23 11:53:09 +0530

    storage issue reported

--- src/main/java/com/service/mobile/config/PaymentOptionConfig.java
+++ src/main/java/com/service/mobile/config/PaymentOptionConfig.java
@@ -3,22 +3,35 @@ package com.service.mobile.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class PaymentOptionConfig {
 
-    @Value("#{${payment.option.currency}}")
-    private Map<String, String> currency;
+    @Value("${payment.option.currency.data}")
+    private String paymentOptionCurrency;
 
-    @Value("#{${payment.option.method}}")
-    private Map<String, String> paymentMethod;
+    @Value("${payment.option.method}")
+    private String paymentOptionMethod;
 
     public Map<String, String> getCurrency() {
-        return currency;
+        Map<String,String> response = new HashMap<>();
+        String[] data = paymentOptionCurrency.split("\\|");
+        for(String value:data){
+            String[] keyValue = value.split(":");
+            response.put(keyValue[0],keyValue[1]);
+        }
+        return response;
     }
 
     public Map<String, String> getPaymentMethod() {
-        return paymentMethod;
+        Map<String,String> response = new HashMap<>();
+        String[] data = paymentOptionMethod.split("\\|");
+        for(String value:data){
+            String[] keyValue = value.split(":");
+            response.put(keyValue[0],keyValue[1]);
+        }
+        return response;
     }
 }
--- src/main/java/com/service/mobile/storage/StorageProperties.java
+++ src/main/java/com/service/mobile/storage/StorageProperties.java
@@ -3,8 +3,8 @@ package com.service.mobile.storage;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
 
-@Data
 @ConfigurationProperties(value = "storage")
 public class StorageProperties {
 
@@ -12,6 +12,12 @@ public class StorageProperties {
      * Folder location for storing files
      */
     @Value("${storage.location}")
-    private String location = "upload-dir";
+    public String location;
+    public String getLocation() {
+        return location;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
